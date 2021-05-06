c3c4f599c314c6db2ca63ee3b9cde6fec58a8faa
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2021-05-06 12:54:46 +0000

    setup the url to the vote page, its templates,as will as its functionality in the views.py

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import get_object_or_404, render,HttpResponse
+from .models import Question,Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('We are live')
+    questions =Question.objects.order_by('-pub_date')
+    context = {
+        'questions':questions
+    }
+    return render(request,'polls/index.html',context)
+    # return HttpResponse('We are live')
 
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    context ={
+        'question':question
+    }
+    return render(request,'polls/detail.html',context)
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    context ={
+        'question':question
+    }
+    return render(request,'polls/detail.html',context)
