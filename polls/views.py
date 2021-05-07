6473ba99a6f5007093dd65fe012dbc67dd8521b6
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2021-05-07 14:29:09 +0000

    finished the script for the vote functionality in the views.py

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render,HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render,HttpResponse
 from .models import Question,Choice
 
 # Create your views here.
@@ -20,7 +20,21 @@ def detail(request,question_id):
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
-    context ={
-        'question':question
-    }
-    return render(request,'polls/detail.html',context)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        context ={ 
+            'question':question,
+            'error_message': "You didn't select a choice."}
+        return render(request,'polls/detail.html',context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results')
+
+
+def results(request):
+    questions = Question.objects.all()
+
+    context ={ 'question':questions}
+    return render(request,'polls/results.html',context)
